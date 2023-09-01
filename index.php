8df70363d5478d48363fd9e3781fcb5b91ce3293
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-01 16:14:00 +0200

    ADD print newobject class

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    class Movie {
+
+    }
+
+
+    $nuovoOggettoUser = new Movie();
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+    <body>
+        <div class="container">
+            <div class="text">
+
+                <?php var_dump($nuovoOggettoUser) ?>
+
+            </div>
+        </div>
+    </body>
+</html>
