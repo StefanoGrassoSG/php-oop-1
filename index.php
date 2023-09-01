11ced77f2b1cffbdbd5cc858ba01b8187eec1c5b
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-01 18:05:16 +0200

    EDIT move to classes folder

--- index.php
+++ index.php
@@ -1,45 +1,6 @@
 <?php
-    class Movie {
-        public $title;
-        public $release;
-        public $vote;
-        public $director;
-        private $avaiable;
-
-        public function getTitleRelease() {
-            return $this->title.' '.$this->release;
-        }
-
-        public function getActive() {
-            return $this->avaiable;
-        }
-
-        public function setActive($avaiable) {
-            if(is_bool($avaiable)) {
-                $this->avaiable = $avaiable;
-            }
-        }
-    }
-
-
-    $batman = new Movie();
-
-    $batman->title = 'the dark Knight';
-    $batman->release = '2008-07-13';
-    $batman->vote = 8;
-    $batman->director = 'Christopher Nolan';
-    $batman->setActive(true);
-
-
-
-    $hulk = new Movie();
-
-    $hulk->title = 'the incredible hulk'
+    require_once __DIR__.'/classes/movie.php';
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
