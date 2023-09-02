df5131e17fc5bb65944bb7620f2f4a20156da72e
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-02 13:03:08 +0200

    EDIT print on bootstrap table

--- database/db.php
+++ database/db.php
@@ -2,7 +2,7 @@
 
 $moviesData = [
     [
-        'title' => 'the dark Knight',
+        'title' => 'The dark Knight',
         'release' => '2008-07-13',
         'vote' => 8,
         'director' => 'Christopher Nolan',
@@ -13,13 +13,48 @@ $moviesData = [
         'avaiable' => true
     ],
     [
-        'title' => 'the incredible hulk',
+        'title' => 'The incredible hulk',
         'release' => '2012-09-15',
         'vote' => 7,
         'director' => 'Ron Howard',
         'genre' => [
             'action',
             'fantasy'
+        ],
+        'avaiable' => false
+    ],
+    [
+        'title' => 'Pulp Fiction',
+        'release' => '1994-07-2',
+        'vote' => 9.5,
+        'director' => 'Quentin Tarantino',
+        'genre' => [
+            'crime',
+            'dramatic'
+        ],
+        'avaiable' => true
+    ],
+    [
+        'title' => 'Interstellar',
+        'release' => '2014-02-18',
+        'vote' => 9,
+        'director' => 'Christopher Nolan',
+        'genre' => [
+            'adventure',
+            'fantasy',
+            'dramatic'
+        ],
+        'avaiable' => true
+    ],
+    [
+        'title' => 'The Martian',
+        'release' => '2015-04-18',
+        'vote' => 8.5,
+        'director' => 'Ridley Scott',
+        'genre' => [
+            'Adventure',
+            'fantasy',
+            'dramatic'
         ],
         'avaiable' => false
     ]
